bcd8add2f7646ae6ff4055c54c7b72b18480ba1f
Waterfox <waterfox80@gmail.com>
2024-04-28 21:54:46 +0200

    🗃️ feat: Add Database Field

--- alex-api/api/server/models.py
+++ alex-api/api/server/models.py
@@ -1,3 +1,111 @@
-from django.db import models
+from django.db.models import Model 
 
-# Create your models here.
+from django.db.models import CharField 
+from django.db.models import DateTimeField
+from django.db.models import IntegerField
+from django.db.models import URLField
+from django.db.models import BooleanField
+from django.db.models import TextField
+from django.db.models import AutoField
+from django.db.models import DateField
+  
+from django.db.models import CASCADE  
+  
+from django.db.models import ForeignKey
+from django.db.models import ManyToManyField    
+
+from api.settings import DATABASE_TABLE_PREFIX
+
+class Author(Model): 
+    
+    class Meta:
+        db_table = f'{DATABASE_TABLE_PREFIX}_author'
+    
+    id = AutoField(primary_key=True)
+    
+    first_name = CharField(max_length=255)
+    name = CharField(max_length=255) 
+    
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True) 
+    active =  BooleanField(default=True)
+    
+
+class Shelf(Model): 
+    
+    class Meta:
+        db_table = f'{DATABASE_TABLE_PREFIX}_shelf'
+    
+    id = AutoField(primary_key=True)
+    
+    name = CharField(max_length=255) 
+    description = TextField(null=True, blank=True)
+        
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True) 
+    active =  BooleanField(default=True)
+    
+
+class BookAvailability(Model): 
+    
+    class Meta:
+        db_table = f'{DATABASE_TABLE_PREFIX}_book_availability'
+    
+    id = AutoField(primary_key=True)
+    
+    name = CharField(max_length=255) 
+        
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True) 
+    active =  BooleanField(default=True)
+
+
+class BookState(Model):
+        
+    class Meta:
+        db_table = f'{DATABASE_TABLE_PREFIX}_book_state'    
+        
+    id = AutoField(primary_key=True)
+    
+    name = CharField(max_length=255) 
+        
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True) 
+    active =  BooleanField(default=True)
+
+
+class Book(Model): 
+    
+    class Meta:
+        db_table = f'{DATABASE_TABLE_PREFIX}_book'
+    
+    id = AutoField(primary_key=True)
+    
+    isbn = IntegerField()
+    title = CharField(max_length=255) 
+    author = ManyToManyField('Author', related_name='books')
+    shelf = ForeignKey('Shelf', related_name='books', on_delete=CASCADE)
+    availability = ForeignKey(BookAvailability, related_name='books', on_delete=CASCADE)
+    state = ForeignKey(BookState, related_name='books', on_delete=CASCADE)
+    
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True) 
+    active =  BooleanField(default=True)
+
+
+class Edition(Model): 
+    
+    class Meta:
+        db_table = f'{DATABASE_TABLE_PREFIX}_edition'
+    
+    id = AutoField(primary_key=True)
+    
+    name = CharField(max_length=255)
+    publisher = CharField(max_length=255)
+    publication_date = DateField()
+    book = ForeignKey('Book', related_name='editions', on_delete=CASCADE)
+    cover = URLField(null=True, blank=True)
+    
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True) 
+    active =  BooleanField(default=True)
