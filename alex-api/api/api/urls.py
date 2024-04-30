fa73e1cf40dbf7d6d66f3de453ec486e9167ad72
Waterfox <waterfox80@gmail.com>
2024-04-30 09:41:09 +0200

    📝 feat(API): Add API Document

--- alex-api/api/api/urls.py
+++ alex-api/api/api/urls.py
@@ -1,24 +1,12 @@
-"""
-URL configuration for api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
 from rest_framework import routers
+from rest_framework import permissions 
+ 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from server.viewset import AuthorViewSet
 from server.viewset import ShelfViewSet
@@ -27,6 +15,16 @@ from server.viewset import BookStateViewSet
 from server.viewset import BookAvailabilityViewSet
 from server.viewset import BookViewSet
 from server.viewset import BookViewSet
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Alex API", 
+        default_version="v1", 
+        description="Alex API is the main interface to interact with Alex database.",  
+        license=openapi.License(name = "CC BY-NC-SA 4.0")
+    )
+)
 
 router = routers.DefaultRouter()
 
@@ -38,9 +36,11 @@ router.register(r'bookavailabilities', BookAvailabilityViewSet, basename='bookav
 router.register(r'books', BookViewSet, basename='book')
 
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),   
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+
 ]
