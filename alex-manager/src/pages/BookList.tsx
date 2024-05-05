29bc49f7c63c40129f58078e790b12db969f5333
Waterfox <waterfox80@gmail.com>
2024-05-05 12:28:44 +0200

    🐛fix(Manager): Add A Modal When API Result Is Null

--- alex-manager/src/pages/BookList.tsx
+++ alex-manager/src/pages/BookList.tsx
@@ -1,11 +1,14 @@
-import { Chip, Sheet, Button } from "@mui/joy";
+import { Chip, Sheet, Button, Modal, ModalClose, Typography, ModalDialog } from "@mui/joy";
 import Table from "@mui/joy/Table";
 
 import Book from "../types/book";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 function BookList() {
+
+  const [open, setOpen] = useState<boolean>(true);
 
   var { data, isLoading, isError } = useQuery({
     queryKey: ['books'],
@@ -15,9 +18,41 @@ function BookList() {
   });
 
   data = data as Book[];
-  
+
   if (isLoading) return <div>Loading...</div>
-  if (isError) return <div>Error...</div>
+  if (isError) return <Modal
+      aria-labelledby="modal-title"
+      aria-describedby="modal-desc"
+      open={open}
+      onClose={() => setOpen(false)}
+      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    >
+    <ModalDialog
+      variant="outlined"
+      sx={{
+        maxWidth: 500,
+        borderRadius: 'md',
+        p: 3,
+        boxShadow: 'lg',
+      }}
+      color="danger"
+    >
+      <ModalClose variant="plain" sx={{ m: 1 }} />
+      <Typography
+        component="h2"
+        id="modal-title"
+        level="h4"
+        textColor="inherit"
+        fontWeight="lg"
+        mb={1}
+      >
+        An error occured while fetching data
+      </Typography>
+      <Typography id="modal-desc" textColor="text.tertiary">
+        Make sure your internet connection is stable and try again. If the problem persists, please contact the system administrator.
+      </Typography>
+    </ModalDialog>
+  </Modal>
 
   return (
     <div>
